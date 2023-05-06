ea7133e3689209a31256d8c209526a8e30b4888e
Fadhil Wicaksono <129974191+rubyfnql@users.noreply.github.com>
2023-05-06 20:15:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function gotoDetail1() {
+	window.location.href = "detail1.html";
+}
+function gotoDetail2() {
+	window.location.href = "detail2.html";
+}
+function gotoDetail3() {
+	window.location.href = "detail3.html";
+}
+function gotoDetail4() {
+	window.location.href = "detail4.html";
+}
